cd48730f67ccf9b498356f5376e80e098074aa19
Sebastião Cosme Agostinho <81954475+scosme20@users.noreply.github.com>
2024-02-28 23:20:18 -0300

    Delete Api/src directory

--- Api/src/Server/server.js
+++
@@ -1,7 +0,0 @@
-import app from '../app.js';
-
-const port = 3500
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta localhost: ${port}`);
-})
--- Api/src/configDb.js
+++
@@ -1,7 +0,0 @@
-import  sqlite3  from "sqlite3";
-import { open }   from 'sqlite3'
-
-sqlite3.verbose();
-export async function openDb () {
-  return open ({})
-};
